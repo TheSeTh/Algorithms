01aa6be1d2d7e304360e8d85e66224a6b2efcf86
Seth Pro <seth.pro@gmail.com>
2013-01-08 23:36:15 +0100

    66,67% faster with memmove

--- Sorting/insertionsort_vet.cpp
+++ Sorting/insertionsort_vet.cpp
@@ -1,6 +1,7 @@
 // Insertion Sort
 
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 #include <string>
 #include <boost/lexical_cast.hpp>
@@ -21,45 +22,41 @@ int main(int argc, char* argv[])
 		std::string filename = argv[2];
 		std::ifstream myfile (filename);
 		t = (boost::lexical_cast<int>(argv[1])>1)?boost::lexical_cast<int>(argv[1]):1;
-		std::cout << "Inizio lettura input:" << std::endl;
 		if(myfile.is_open())
 		{
 			std::getline(myfile,line);
 			n = boost::lexical_cast<int>(line);
 			std::getline(myfile,line);
+			x = 0;
+			A.resize(n);
 			while(myfile.good())
 			{
-				A.push_back(boost::lexical_cast<int>(line));
+				A[x]=boost::lexical_cast<int>(line);
 				std::getline(myfile,line);
+				x++;
 			}
 
 		}
 		myfile.close();
-		std::cout << "Fine lettura input." << std::endl;
 		x = 0;
+		temp.resize(n);
 		while(x<t)
 		{
 			temp = A;
-			/*{for(i=0;i<temp.size();i++)
-				std::cout << temp[i] << " ";
-			std::cout << std::endl;}*/
 			clock_t start = clock();
 			for(j=1;j<temp.size();j++)
 			{
 				key = temp[j];
 				i = j-1;
 				while(i>=0 && temp[i]>key)
-				{
-					temp[i+1] = temp[i];
 					i--;
-				}
+				std::memmove (&temp.front() + (i + 2), &temp.front() + (i + 1), sizeof (int) * ((j - 1) - i));
 				temp[i+1]=key;
 			}
 			clock_t end = clock();
-			/*{for(i=0;i<temp.size();i++)
-				std::cout << temp[i] << " ";
-			std::cout << std::endl;}*/
 			res.open("result_ins1.txt",std::fstream::out | std::fstream::app);
+			res.setf(std::ios::fixed,std::ios::floatfield);
+			res.precision(4);
 			res << double(end-start) / CLOCKS_PER_SEC << std::endl;
 			res.close();
 			x++;
